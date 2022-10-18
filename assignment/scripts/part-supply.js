e26e5c7d2a1c880e08894e8fa975f1b018417b39
skydolson <skyolson@gmail.com>
2022-10-18 17:03:35 -0500

    Turning in homework

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -3,21 +3,30 @@ console.log('****** Part Supply *******');
 
 // 1. Create a variable called 'partsNeeded' with a value of the number 40 
 //    & console.log the variable
-console.log('1. Number of partsNeeded:');
+partsNeeded = 40;
+console.log('1. Number of partsNeeded:', partsNeeded);
 
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
-console.log('2. Array of supplyChanges:');
+
+let supplyChanges = [3,5,-6,0,7,11];
+console.log('2. Array of supplyChanges:', supplyChanges);
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
-console.log('3. Second supplyChange is:');
+
+let secondItem = supplyChanges[1];
+console.log('3. Second supplyChange is:', secondItem);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
-console.log('4. Removed item:');
+
+let removedItem = supplyChanges.pop()
+console.log('4. Removed item:', removedItem);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
-console.log('5. Adding 25 to supplyChanges.');
+
+supplyChanges.push(25)
+console.log('5. Adding 25 to supplyChanges.',supplyChanges);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -25,6 +34,19 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
+
+
+for (x of supplyChanges) {
+    if (x > 0) {
+        console.log('Added ' + x + ' parts.')
+    } else if (x == 0) {
+        console.log('No Change.')
+    } else {
+        console.log('Removed ' + -x + ' parts.')
+    }
+        
+    }
+
 
 
 // STRETCH GOALS
